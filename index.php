78b51a1c02f30d8b8ce70219a9c37b6b47bb0830
MuhamadBayuPratama <pbayu4223@gmail.com>
2024-10-17 11:39:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, dicta recusandae mollitia modi, nemo consectetur cum amet ad deserunt libero autem hic culpa delectus! Maiores temporibus quidem aspernatur nihil et.</p>
-    
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
